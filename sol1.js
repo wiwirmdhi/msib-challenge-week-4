e4649f7603b111eb2d2e49d696a19203632449a2
Wiwi Rahmadani <wiwirahmadani03@gmail.com>
2023-09-13 13:48:26 +0700

    latihan

---
+++ sol1.js
@@ -0,0 +1,8 @@
+const people = [
+    { id: 1, name: 'Alice', age: 30 },
+    { id: 2, name: 'Bob', age: 25 },
+    { id: 3, name: 'Charlie', age: 35 },
+  ];
+
+  const names = people.map(person => person.name)
+  console.log(names);
